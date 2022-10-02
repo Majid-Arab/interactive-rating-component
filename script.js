56c22cf645529aa60b5e40bb90cba8cdb083b527
Majid-Arab <arabmajid12@gmail.com>
2022-10-02 21:29:58 +0500

    99% done some fixes left

--- script.js
+++ script.js
@@ -7,7 +7,28 @@ submit.addEventListener('click', function(e){
 
 });
 
-let btn = document.querySelector(".starBtn").value
-btn.addEventListener('click', function(e) {
-    console.log("SMTH" + e)
-} )
+// let btn = getElementsByTagName("li").value
+// btn.addEventListener('onChange', function(e) {
+//     console.log("SMTH" + e)
+// } )
+
+// function list(e) {
+//     console.log(e.value + "THis")
+// }
+
+
+
+const lis = document.querySelectorAll("li"); // selecting all the li's
+const span = document.querySelectorAll("span"); // selecting all the li's
+
+function liValues(e){
+  const target = e.target.innerHTML;
+  console.log(target)
+//   document.write("My"); // or innerHTML or whatever works
+
+  console.log(span)
+}  
+
+lis.forEach((el) => {
+el.addEventListener("click", liValues); // loops through all the li's and bind the function to them
+});
